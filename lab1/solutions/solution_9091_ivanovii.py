5246a0a9fe4c30ec734276cd2f62a801d87e06ae
vtroc7 <vtroc7@gmail.com>
2024-08-29 06:33:41 +0300

    remove creating views

--- lab1/solutions/solution_9091_ivanovii.py
+++ lab1/solutions/solution_9091_ivanovii.py
@@ -1,5 +1,5 @@
 from pyspark.sql import DataFrame
-from pyspark.sql import functions as F, Window
+from pyspark.sql import functions as F
 
 from lab1.common import read_csv, view
 
@@ -10,13 +10,9 @@ from lab1.common import read_csv, view
 #
 
 def solve() -> DataFrame:
-    match = read_csv('match')
-    player = read_csv('player')
-    player_result = read_csv('player_result')
-
-    view("match", match)
-    view("player", player)
-    view("player_result", player_result)
+    match: DataFrame = read_csv('match')
+    player: DataFrame = read_csv('player')
+    player_result: DataFrame = read_csv('player_result')
 
     kda = player_result.groupBy('player_id').agg(
         (F.mean('gold') / 1000).alias('avg_gold'),
